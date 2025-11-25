a0d37272c0c1b431937bc6f716377f04c53dc808
jtdbrab <jules.db.dev@gmail.com>
2025-11-25 22:05:02 +0100

    slow but sure. Add working user choices that just print text, add basic book adding. Laying out the skeleton and getting used to writing a full program from scratch stuff

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -12,15 +12,43 @@ class UserInterface():
             print(f"""
             Welcome to [bold]Paper & Grooves[/bold], {self.username}
             What would you like to do?
-            1. Add book
-            2. Remove book
+            1. Check library
+            2. Add item
+            2. Remove item
             3. Search
             4. Quit
             """)
-            user_choice = int(input("enter a number: "))
-            if (user_choice == 1):
+            user_choice = input("enter a number: ")
+            if (user_choice == "1"):
+                print("this will be good")           
+            elif (user_choice == "2"):
+                self.add_item()
+            elif (user_choice == "3"):
                 print("this will be good")
+            elif (user_choice == "4"):
                 running = False
+            else:
+                print("Not a valid choice")
+    
+    def add_item(self):
+        toAdd = input("What would you like to add (book/record)? ")
+        if toAdd.lower() == "book":
+            print("A BOOK!")
+            return
+        if toAdd.lower() == "record":
+            print("A RECORD!")
+            return
+        else:
+            print("Not a valid choice!")
+            return
+
+    
+
+        
+
+
+ 
+
 
 
 
